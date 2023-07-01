a677c775d7f5c21b13f752d56a2d8702f17b1c90
mfts2048 <18267094443@163.com>
2023-07-01 10:42:29 +0800

    1.0.0 特定环境下的 m3u8 文件下载

---
+++ examples.ts
@@ -0,0 +1,4 @@
+import { createM3u8Download_v1 } from './src'
+
+createM3u8Download_v1('34290')
+  .catch(console.error)
---
+++ src/server.ts
@@ -0,0 +1,75 @@
+import path from 'node:path'
+import { Buffer } from 'node:buffer'
+import { existsSync } from 'node:fs'
+import type { CipherCCMTypes } from 'node:crypto'
+import crypto, { createDecipheriv } from 'node:crypto'
+import { writeFile } from 'node:fs/promises'
+import { exec } from 'node:child_process'
+import { PromisePool } from '@supercharge/promise-pool'
+import download from 'download'
+import ProgressBar from 'progress'
+
+import { createContext } from './init'
+
+export async function createM3u8Download_v1(uid: string) {
+  const _ctx = await createContext(uid)
+
+  const bar = new ProgressBar('[:bar] :current/:total :percent :etas', {
+    total: _ctx.segments.length,
+    width: 20,
+  })
+
+  const { errors, results } = await PromisePool
+    .for(_ctx.segments)
+    .withConcurrency(_ctx.concurrency)
+    .useCorrespondingResults()
+    .onTaskFinished(() => bar.tick())
+    .process(async (segment) => {
+      const filename = path.parse(segment.uri).name
+      const filepath = path.join(_ctx.folder, `${filename}.ts`)
+
+      if (existsSync(filepath)) return filepath
+
+      try {
+        const stream = await download(segment.uri)
+        const iv = crypto.randomBytes(16)
+        const algorithm: CipherCCMTypes = `${segment.key.method}-cbc`.toLowerCase() as CipherCCMTypes
+        const cipher = createDecipheriv(algorithm, _ctx.aes, iv)
+        cipher.on('error', console.error)
+        const segmentData = Buffer.concat([cipher.update(stream), cipher.final()])
+
+        await writeFile(filepath, segmentData)
+        return filepath
+      }
+      catch (err) {
+        console.error(err)
+      }
+
+      return filepath
+    })
+
+  if (errors.length === 0) {
+    await writeFile(
+      _ctx.filepath_merge_temp,
+      results.filter(Boolean).map(url => `file '${url as string}'`).join('\r'),
+      'utf-8',
+    )
+
+    return new Promise((resolve) => {
+      console.log('合并文件中...')
+      console.time('ffmpeg 合并耗时')
+      exec(
+        `ffmpeg -f concat -safe 0 -i ${_ctx.filepath_merge_temp} -c copy -y ${_ctx.filepath_mp4}`,
+        (err) => {
+          err && console.error(`exec error: ${err}`)
+          console.log('合并完成')
+          console.timeEnd('ffmpeg 合并耗时')
+          resolve(_ctx.filepath_mp4)
+        },
+      )
+    })
+  }
+  else {
+    throw errors[0]
+  }
+}
